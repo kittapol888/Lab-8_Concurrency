a67b3498a6dcfd35cbb1eae883ac6e727214e7c9
kittapol888 <kittapol.sa@ku.th>
2025-08-21 19:34:13 +0700

    Create Withdrawer.java

---
+++ Withdrawer.java
@@ -0,0 +1,19 @@
+public class Withdrawer implements Runnable{
+
+    BankAccount tmp ;
+    long value ;
+    int round;
+
+    public Withdrawer(BankAccount tmp, long value, int round){
+        this.tmp = tmp ;
+        this.value = value ;
+        this.round = round ;
+    }
+
+    public void run() {
+        for (int i = 0; i < round; i++) {
+            tmp.withdraw(value);
+        }
+    }
+    
+}
